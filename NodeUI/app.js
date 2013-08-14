c9db2015a65a33b51984c02969af70ce3f19905c
monkeybutter <p.rozas.larraondo@gmail.com>
2013-08-14 14:35:45 +1000

    Added daterange Post method to NodeUI

--- NodeUI/app.js
+++ NodeUI/app.js
@@ -5,7 +5,6 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
@@ -31,6 +30,12 @@ if ('development' == app.get('env')) {
 app.get('/', function(req, res){
    res.render('home.html')
 });
+
+app.post('/daterange', routes.findDateRange);
+
+app.get('/hoe', routes.index);
+
+app.get('/ui', routes.findObsByInterval);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- NodeUI/routes/index.js
+++ NodeUI/routes/index.js
@@ -1,8 +1,53 @@
+var mongo=require('mongodb');
 
-/*
- * GET home page.
- */
+var Server = mongo.Server,
+    Db = mongo.Db,
+    BSON = mongo.BSONPure;
+  
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+db = new Db('solarPod', server);
+ 
+db.open(function(err, db) {
+   if(!err) {
+     console.log("Connected to solarPod database");
+   }
+});
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index.html');
+};
+
+
+exports.findDateRange = function(req, res){
+   var station = "Namadgi School"
+   var params = req.body;
+   console.log(params);
+   
+   db.collection('obs', function(err, collection) {
+       collection.find({"name": station}, {"sort": [['field1','asc']]}, {"limit": 1}, function(err, cursor) {
+          cursor.toArray(function(err, items) {
+	         res.json(items);
+	         res.end();
+	      }); 
+	   }); 
+   }); 
+};
+
+
+exports.findObsByInterval = function(req, res){
+   var station = "Namadgi School"
+   var date1 = new Date(2012, 6, 1, 0, 0, 0, 0);
+   var date2 = new Date(2012, 7, 1, 0, 0, 0, 0);
+   var params = req.body;
+
+   console.log(params);
+   
+   db.collection('obs', function(err, collection) {
+       collection.find({"name": station, "date": { $gt: date1, $lte: date2 } }, function(err, cursor) {
+          cursor.toArray(function(err, items) {
+	         res.json(items);
+	         res.end();
+	      }); 
+	   }); 
+   }); 
 };
