2262537aa9f99ac35f6c0909b7b83ca1ab78ae14
Pablo Rozas Larraondo <roz016@arena-ids-cdc.it.csiro.au>
2013-08-22 16:28:41 +1000

    Change Bootstrap template for white

--- NodeUI/routes/index.js
+++ NodeUI/routes/index.js
@@ -5,7 +5,7 @@ var Server = mongo.Server,
     BSON = mongo.BSONPure;
   
 var server = new Server('localhost', 27017, {auto_reconnect: true});
-db = new Db('solarPod', server);
+db = new Db('solar-isd', server);
  
 db.open(function(err, db) {
    if(!err) {
@@ -19,12 +19,12 @@ exports.index = function(req, res){
 
 
 exports.findDateRange = function(req, res){
-   var station = "Namadgi School"
+   var station = 91148
    var params = req.body;
    console.log(params);
    
-   db.collection('obs', function(err, collection) {
-       collection.find({"name": station}, {"sort": [['field1','asc']]}, {"limit": 1}, function(err, cursor) {
+   db.collection('minute', function(err, collection) {
+       collection.find({"stationCode": station}, {"sort": [['field1','asc']]}, {"limit": 1}, function(err, cursor) {
           cursor.toArray(function(err, items) {
 	         res.json(items);
 	         res.end();
@@ -35,15 +35,15 @@ exports.findDateRange = function(req, res){
 
 
 exports.findObsByInterval = function(req, res){
-   var station = "Namadgi School"
-   var date1 = new Date(2012, 6, 1, 0, 0, 0, 0);
-   var date2 = new Date(2012, 7, 1, 0, 0, 0, 0);
+   var station = 91148
+   var date1 = new Date(2010, 6, 1, 0, 0, 0, 0);
+   var date2 = new Date(2010, 7, 1, 0, 0, 0, 0);
    var params = req.body;
 
    console.log(params);
    
-   db.collection('obs', function(err, collection) {
-       collection.find({"name": station, "date": { $gt: date1, $lte: date2 } }, function(err, cursor) {
+   db.collection('minute', function(err, collection) {
+       collection.find({"stationCode": station, "date": { $gt: date1, $lte: date2 } }, function(err, cursor) {
           cursor.toArray(function(err, items) {
 	         res.json(items);
 	         res.end();
